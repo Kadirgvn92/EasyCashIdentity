c0d5157567fb16a7144b5db5ddbfc6bf6eb95c9a
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-25 15:10:12 +0300

    Lesson 3 : Customer Accounts created

---
+++ EasyCashIdentity.EntityLayer/Concrete/CustomerAccount.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EasyCashIdentity.EntityLayer.Concrete;
+public class CustomerAccount
+{
+    public int CustomerAccountID { get; set; }
+    public string CustomerAccountNumber { get; set; }
+    public string CustomerAccountCurrency { get; set; }
+    public string CustomerAccountBalance { get; set; }
+    public string BankBranch { get; set; }
+}
