d8ad778409ef6da098d6e3860966e75dc353c13f
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-27 07:50:56 +0300

    Lesson 10 : RegisterController created and Index created

---
+++ EasyCashIdentity.PresentationLayer/Controllers/RegisterController.cs
@@ -0,0 +1,42 @@
+﻿using EasyCashIdentity.DtoLayer.Dtos.AppUserDtos;
+using EasyCashIdentity.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentity.PresentationLayer.Controllers;
+public class RegisterController : Controller
+{
+    private readonly UserManager<AppUser> _userManager;
+
+    public RegisterController(UserManager<AppUser> userManager)
+    {
+        _userManager = userManager;
+    }
+    [HttpGet]
+    public IActionResult Index()
+    {
+        return View();
+    }
+    [HttpPost]
+    public async Task<IActionResult> Index(AppUserRegisterDto appUserRegisterDto)
+    {
+        if(ModelState.IsValid)
+        {
+            AppUser  appUser = new AppUser()
+            {
+                UserName = appUserRegisterDto.Username,
+                Name = appUserRegisterDto.Name,
+                Email = appUserRegisterDto.Email,
+                Surname = appUserRegisterDto.Surname
+            };
+
+            var result = await _userManager.CreateAsync(appUser, appUserRegisterDto.Password);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index","ConfirmMail");
+            }
+        }
+
+        return View();
+    }
+}
