93526d0fecd4ab600926524ac2e278c307191725
Kadirgvn92 <kdrgvndeveloper@gmail.com>
2023-12-28 20:29:30 +0300

    SOme progress done

---
+++ EasyCashIdentity.PresentationLayer/Controllers/MyAccountsController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EasyCashIdentity.PresentationLayer.Controllers;
+public class MyAccountsController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
